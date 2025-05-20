d6947204b4bdff9282aea40df38a063cb7b033fd
Your Name <you@example.com>
2025-05-20 12:14:45 +0200

    full acabado

--- resources/views/instrucciones.blade.php
+++ resources/views/instrucciones.blade.php
@@ -8,7 +8,7 @@
             <h3>{{ __('general.paso1_titulo') }}</h3>
             <p>{{ __('general.paso1_descripcion') }}</p>
         </div>
-        <img src="/imgs/scan2.jpg" alt="Paso 1">
+        <img src="/imgs/ins1.png" alt="Paso 1">
     </div>
 
     <div class="step">
@@ -26,7 +26,7 @@
             <h3>{{ __('general.paso3_titulo') }}</h3>
             <p>{{ __('general.paso3_descripcion') }}</p>
         </div>
-        <img src="/imgs/paso3.png" alt="Paso 3">
+        <img src="/imgs/inst3.jpg" alt="Paso 3">
     </div>
 </div>
 @endsection
